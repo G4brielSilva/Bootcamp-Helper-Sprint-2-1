4d9a00f87c3dfdb28e4b2d515e6c0ad2322d2309
Silva <GabrielSilva@gmail.com>
2021-11-09 16:47:51 -0300

    Correção para métodos estáticos

--- app.js
+++ app.js
@@ -2,21 +2,17 @@
 /*5 - Criar uma classe que retorne a quantidade de vowels da palavra passada.
   a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
   b) Dar um exemplo de uso com uma palavra recebida via input no formulário.*/
-class WordtoCount {
-    constructor(w) {
-        this.str = w;
-    }
-    normalizeString(wd) {
+class Word {
+    static normalizeString(wd) {
         return wd.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
     }
-    countVowels(word) {
+    static countVowels(word) {
         const letters = Array.from(word);
         return letters.filter((letter) => 'aeiou'.includes(this.normalizeString(letter))).length;
     }
 }
-let p1 = new WordtoCount('AdalbÉrto');
 function contaVogal() {
-    p1.str = (document.getElementById("wd").value).toLowerCase();
-    console.log(`A palavra ${p1.str} tem ${p1.countVowels(p1.str)} vogais`);
+    const str = (document.getElementById("wd").value);
+    console.log(`A palavra ${str} tem ${Word.countVowels(str)} vogais`);
 }
-console.log(`A palavra ${p1.str} tem ${p1.countVowels(p1.str)} vogais`);
+console.log(`A palavra tem ${Word.countVowels("Árvore")} vogais`);
--- app.ts
+++ app.ts
@@ -2,28 +2,20 @@
   a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
   b) Dar um exemplo de uso com uma palavra recebida via input no formulário.*/
 
-class WordtoCount {
-    
-    public str: string;
-    constructor(w: string){
-        this.str = w;
-    }
-
-    private normalizeString(wd: string): string{
+class Word {
+    private static normalizeString(wd: string): string{
         return wd.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
     }
 
-    public countVowels(word: string): number {
+    public static countVowels(word: string): number {
         const letters: Array<string> = Array.from(word);
         return letters.filter((letter: string) => 'aeiou'.includes(this.normalizeString(letter))).length;
     }
 }
 
-let p1 = new WordtoCount('AdalbÉrto');
-
-
 function contaVogal(): void{
-    p1.str= ((document.getElementById("wd") as HTMLInputElement).value).toLowerCase();
-    console.log(`A palavra ${p1.str} tem ${p1.countVowels(p1.str)} vogais`);
+    const str: string= ((document.getElementById("wd") as HTMLInputElement).value);
+    console.log(`A palavra ${str} tem ${Word.countVowels(str)} vogais`);
 }
-console.log(`A palavra ${p1.str} tem ${p1.countVowels(p1.str)} vogais`);
+
+console.log(`A palavra tem ${Word.countVowels("Árvore")} vogais`);
